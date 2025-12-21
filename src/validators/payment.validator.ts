1125d496a609c52ad70bd74dace5caf37f22674d
Annas Putra Anuraga <annasputra52@gmail.com>
2025-12-22 05:28:31 +0700

    Add createPaymentSchema with additional payment methods and required fields

--- src/validators/payment.validator.ts
+++ src/validators/payment.validator.ts
@@ -29,3 +29,25 @@ export const partnerSchema = Joi.object({
     cap: Joi.number().min(0).optional().allow(null)
   }).default({})
 });
+
+export const createPaymentSchema = Joi.object({
+  amount: Joi.number().min(1000).required(),
+  // 👇 NAH INI DIA BIANG KEROKNYA, TAMBAHIN KODE LAIN DISINI
+  payment_method: Joi.string().valid(
+    'BCA_VA', 
+    'BNI_VA', 
+    'BRI_VA',      // <--- Tambahin ini
+    'MANDIRI_VA',  // <--- Tambahin ini
+    'PERMATA_VA',  // <--- Tambahin ini
+    'OVO',         // <--- Tambahin ini
+    'DANA',        // <--- Tambahin ini
+    'GOPAY',       // <--- Tambahin ini
+    'QRIS'         // <--- Tambahin ini
+  ).required(),
+  
+  customer_name: Joi.string().required(),
+  customer_email: Joi.string().email().required(),
+  customer_phone: Joi.string().optional(),
+  reference_id: Joi.string().required(),
+  description: Joi.string().optional()
+});
